80dddaff244f979ed69d24f3259bb877cce19649
frankyangkun <776503267@qq.com>
2022-09-22 16:36:45 +0800

    Update tools.groovy
    新增可以指定打印信息的颜色

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,6 +1,11 @@
 package org.devops
 
  //打印内容
-def PrintMes(content){
-  println(content)
+def PrintMes(value,color){
+ colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${value} \033[0m",
+              'green' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+    ansiColor('xterm') {
+        println(colors[color])
+    }
 }
