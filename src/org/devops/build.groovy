b66b8afc777349e4aa9157021dae4b3169ffbc09
frankyangkun <776503267@qq.com>
2022-10-27 08:38:51 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -12,8 +12,17 @@ def Build(buildType,buildShell){
     sh "${buildType} ${buildShell}"
 }
 
-//获取url中的项目名
-def getProjectName(url){
+def getProjectName(url){//保留.git
+    splitStr1 = url.split('/')
+
+    println("getProjectName按/分割结果gitlab：${splitStr1}")
+    println("项目名称："+splitStr1[-1])//只能这样写才可获取列表中的数据，-1表示最后一个
+    
+    return splitStr1[-1]
+}
+
+//获取url中的项目名（去掉.git后缀）
+def getProjectName2(url){
     //http://131.10.11.94:31154/root/044-springboot-dubbo-ssm-parent.git
     splitStr1 = url.split('/')
     //splitStr2 = url.tokenize('.')
@@ -23,7 +32,28 @@ def getProjectName(url){
     //println("项目名称：${splitStr1}[-1]") //无法从结果中进一步取值
     //println("项目名称：${splitStr1}[4]") //无法从结果中进一步取值
     println("项目名称："+splitStr1[-1])//只能这样写才可获取列表中的数据，-1表示最后一个
-    return splitStr1[-1]
+    
+    //去掉.git后缀
+    //projectNameList = splitStr1[-1].split('.')//得到的projectNameList是空白
+    splitStr2 = splitStr1[-1]
+    projectNameList = splitStr2.tokenize('.')
+    println("projectNameList:"+projectNameList)//得到的projectNameList也是空白，换成tokensize分割就可以，不知道为啥
+    
+    projectName = projectNameList[0]
+    println("项目名称去掉.git后："+projectName)
+    
+    return projectName
     //println("getProjectName按.分割结果gitlab：${splitStr2}")
    // println(splitStr2) //用tokensize分割的，打印的是实际内容
+}
+
+def modifyProjectDirName(url,projectName){
+    sh "pwd"
+    splitStr1 = url.split('/')//列表中包含.git
+    splitStr2 = splitStr1[-1] 
+    println("保留.git的项目名:"+splitStr2)
+    
+    sh "mv ${splitStr2} ${projectName}"
+    println("目录名修改完成")
+    sh "pwd"
 }
